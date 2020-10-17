6a612b646b5c8d6187d17dc43c98f5681a3a8a47
Soin Na <carrotcold@gmail.com>
2020-10-17 23:31:17 +0900

    ~68. Filtering Paths

--- app.js
+++ app.js
@@ -3,33 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs!');
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  console.log('여기는 /add-product');
-  res.send(
-    '<form action="/product" method="POST">\
-  <input type = "text" name = "title">\
-    <button type="submit">Add Product</button>\
-  </input >\
-    </form>'
-  );
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-  console.log('여기는 /product');
-  console.log(req.body); // undefined
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  console.log('여기는 /');
-  res.send('<h1>HELLO from EXPRESS</h1>');
+app.use((req, res, next) => {
+  res.status(404).send('<h1>404 Page Not Found</h1>');
 });
 
 app.listen(3000, console.log(`http://localhost:3000/`));
