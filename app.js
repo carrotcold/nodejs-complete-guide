0cc03d42f06a91a1daf31644955cbecd695ef516
Soin Na <carrotcold@gmail.com>
2020-10-19 04:08:18 +0900

    change to ejs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -17,7 +17,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found!' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found!', path: null });
 });
 
 app.listen(3000, console.log(`http://localhost:3000/`));
--- routes/shop.js
+++ routes/shop.js
@@ -6,10 +6,14 @@ const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
+
   res.render('shop', {
+    prods: products,
     pageTitle: 'Shop',
-    products,
     path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
   });
 });
 
