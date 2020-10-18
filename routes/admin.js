a36fcf3af256e43ad5e1816093c5ce921bd97732
Soin Na <carrotcold@gmail.com>
2020-10-19 02:02:14 +0900

    ~74. Serving Files Statically

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 // GET : /admin/add-product
 router.get('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST">\
-  <input type = "text" name = "title">\
-    <button type="submit">Add Product</button>\
-  </input >\
-    </form>'
-  );
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // POST : /admin/add-product
